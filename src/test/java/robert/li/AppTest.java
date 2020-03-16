ee1fa0c9cb7f79fede1345307136fcd0c2b27aa2
robert.li <robert.li@qunar.com>
2020-03-16 09:51:41 +0800

    A

--- src/test/java/robert/li/AppTest.java
+++ src/test/java/robert/li/AppTest.java
@@ -17,4 +17,22 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        // memo[i] 表示 以memo[i]为结尾的最长上升子序列 长度
+        int[] memo = new int[n];
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int max = 0;
+            for (int j = 0; j < i; j++) {
+                if(nums[j] < nums[i]) {
+                    max = Math.max(max, memo[j]);
+                }
+            }
+            memo[i] = max + 1;
+            res = Math.max(res, memo[i]);
+        }
+        return res;
+    }
 }
