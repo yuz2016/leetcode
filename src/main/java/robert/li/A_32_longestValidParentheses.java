5ecbf93673e55a6f5c51b74722fc3e4bd77bcf2b
robert.li <robert.li@qunar.com>
2020-03-20 14:51:17 +0800

    A

---
+++ src/main/java/robert/li/A_32_longestValidParentheses.java
@@ -0,0 +1,29 @@
+package robert.li;
+
+/**
+ * @author robert.li
+ * @version v1.0.0
+ * @since 2020-03-20
+ */
+public class A_32_longestValidParentheses {
+
+    public int longestValidParentheses(String s) {
+        if(s.length() == 0) return 0;
+        char[] chars = s.toCharArray();
+        int res = 0;
+        int[] dp = new int[chars.length];
+        for (int i = 1; i < chars.length; i++) {
+            if(chars[i] == ')') {
+                if(chars[i - 1] == '(') {
+                    dp[i] = (i - 2 >= 0)? dp[i - 2] + 2:2;
+                } else {
+                    if(dp[i - 1] != 0 && i - dp[i - 1] > 0 && chars[i - dp[i - 1] - 1] == '(') {
+                        dp[i] = dp[i - 1] + 2 + ((i - dp[i - 1] - 1 > 0) ? dp[i - dp[i - 1] - 2] : 0);
+                    }
+                }
+                res = Math.max(res, dp[i]);
+            }
+        }
+        return res;
+    }
+}
