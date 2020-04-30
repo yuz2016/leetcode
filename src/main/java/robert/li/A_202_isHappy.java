b1b4674745ff23fdf23939dfdc6d373d5aff9d5d
robert.li <robert.li@qunar.com>
2020-04-30 11:06:51 +0800

    A

---
+++ src/main/java/robert/li/A_202_isHappy.java
@@ -0,0 +1,28 @@
+package robert.li;
+
+/**
+ * @author robert.li
+ * @version v1.0.0
+ * @since 2020-04-30
+ */
+public class A_202_isHappy {
+    public boolean isHappy(int n) {
+        int one = n;
+        int two = n;
+        do {
+            one = intSquareNum(one);
+            two = intSquareNum(intSquareNum(two));
+        } while(one != two);
+        return one == 1;
+    }
+
+    int intSquareNum(int num) {
+        int res = 0;
+        while(num != 0) {
+            int tmp = num % 10;
+            num = num / 10;
+            res += tmp * tmp;
+        }
+        return res;
+    }
+}
