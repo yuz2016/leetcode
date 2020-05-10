0bf70aa82dacff88e2c918fcfecc7a54ffe7d94b
robert.li <robert.li@qunar.com>
2020-05-10 14:12:15 +0800

    A

---
+++ src/main/java/robert/li/bsolution/B_236_lowestCommonAncestor.java
@@ -0,0 +1,20 @@
+package robert.li.bsolution;
+
+import robert.li.common.TreeNode;
+
+/**
+ * @author robert.li
+ * @version v1.0.0
+ * @since 2020-05-10
+ */
+public class B_236_lowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left == null && right == null) return null;
+        if(left == null && right != null) return right;
+        if(left != null && right == null) return left;
+        return root;
+    }
+}
